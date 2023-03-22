674a67037a9c26d0f213cced2e98f7aea1cf0584
Chethaka Lakshitha <72698655+ChethakaL@users.noreply.github.com>
2023-03-22 16:32:31 +0530

    User Profile
    User Profile data fetch

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { authUser, registerUser, getUserProfile} = require('../controllers/userController');
-
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.post("/login", authUser);
-router.get("/profile", getUserProfile);
+router.get("/profile",protect,getUserProfile)
+
 module.exports = router;
